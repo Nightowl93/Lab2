4a6613ed4e4588bb6395943664a2bccf8761c24c
Nightowl93 <shontini@umich.edu>
2018-07-17 13:36:12 -0400

    Added comments

--- src/co/grandcircus/Lab2/Lab2.java
+++ src/co/grandcircus/Lab2/Lab2.java
@@ -6,6 +6,7 @@ public class Lab2 {
 
 	public static void main(String[] args) {
 		
+		//Declares variables and creates scanner
 		Scanner scnr = new Scanner(System.in);
 		double length;
 		double width;
@@ -17,10 +18,13 @@ public class Lab2 {
 		String keepGoing;
 		Boolean response = true;
 		
+		//Welcome message to user
 		System.out.println("Let's calculate Grand Circus room dimensions!");
 		
+		//Only runs when user chooses to continue
 		while (response == true) {
 
+			//Prompts user for the necessary questions to solve for room area, volume, and perimeter
 			System.out.println("Which classroom are you in? ");
 			classroom = scnr.nextLine();
 			
@@ -33,24 +37,30 @@ public class Lab2 {
 			System.out.println("Please enter the height of the classroom: ");
 			height = scnr.nextDouble();
 			
+			//Calculates and assigns the area, volume, and perimeter to appropriate variables
 			area = length * width;
 			perimeter = 2 * (length + width);
 			volume = length * width * height;
 			
+			//Prints out the calculated values
 			System.out.println("The area of the " + classroom + " classroom is " + area);
 			System.out.println("The perimeter of the " + classroom + " classroom is " + perimeter);
 			System.out.println("The volume of the " + classroom + " classroom is " + volume);
 			
-			//When loop reiterates, this ensures this question will not be skipped over. Debugged.
+			//When loop reiterates, this ensures this question will not be skipped over. 
 			scnr.nextLine();
+			
+			//Prompts user to continue or not
 			System.out.println("Would you like to continue? (y/n) ");
 			keepGoing = scnr.nextLine();
 			
+			//If user continues, ensures loop reiterates
 			if (keepGoing.charAt(0) == 'y' || keepGoing.charAt(0) == 'Y')
 			{
 				response = true;
 			}
 			
+			//If user does not wish to continue, ensures loop does not reiterate and tells user goodbye
 			else {
 				response = false;
 				System.out.println("Thank you and Goodbye!");
@@ -61,8 +71,3 @@ public class Lab2 {
 	}
 
 }
-
-
-//Area = lw
-//perimeter = 2 (l+w) 
-//volume = lwh
